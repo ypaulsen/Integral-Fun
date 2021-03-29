f5f8366d17e078b35083129ab91f3a91fe634ee5
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-03-29 01:35:45 -0700

    Update Monte Carlo Integration.R

--- Monte Carlo Integration.R
+++ Monte Carlo Integration.R
@@ -2,17 +2,17 @@
 
 # EG Normal distribution   
 
-# The following program uses Acceptance/Rejection integration to calculate the 
-# area under the curve of the standard normal distribution over the interval 
-# (-1.96, 1.96). This integral is well known. While there is no exact closed 
-# form solution for this integral over an arbitrary range, the practiced 
-# statistician will recognize that this interval contains 95% of a probability 
-# density function and therefore the exact solution is known as 0.95.   
+# The following program uses Monte Carlo integration to calculate the area under
+# the curve of the standard normal distribution over the interval (-1.96, 1.96).
+# This integral is well known. While there is no exact closed form solution for 
+# this integral over an arbitrary range, the practiced statistician will 
+# recognize that this interval contains 95% of a probability density function and
+# therefore the exact solution is known as 0.95.   
 
-# This program performs Acceptance/Rejection integration on the function defined
-# as f() over the interval (a, b). I have used the normal distribution in this 
-# example over a well studied interval for demonstration purposes since it has 
-# a widely known and predictable outcome.  
+# This program performs Monte Carlo integration on the function defined as f() 
+# over the interval (a, b). I have used the normal distribution in this example 
+# over a well studied interval for demonstration purposes since it has a widely 
+# known and predictable outcome.  
 
 
 # Function  
@@ -37,16 +37,13 @@ set.seed(pi)
 error <- c()
 
 for(i in 1:100){
-  x <- runif(n, a, b)
-  y <- runif(n, 0, max)
-  p <- mean(y < f(x))
-  A <- (b-a)*max
-  I <- p*A
+  x <- runif(MC, a, b)
+  I <- (b-a)*mean(f(x))
   error[i] <- abs(0.95 - I)
 }
 
 mean(error)
 
 
-# With the supplied seed, the mean absolute error is just 0.00062. Or around 
-# 0.062%.  
+# With the supplied seed, the mean absolute error is just 9.275671e-05. Or around 
+# 0.000093%.    
